8cc5bf9500b3e1de234e345d606a4a9d2a8c2349
Elizabeth-Meliani <melzyunho@gmail.com>
2025-09-21 17:20:06 +0700

    update

--- app.py
+++ app.py
@@ -12,67 +12,65 @@ df = pd.read_csv("Laptop_price.csv")
 
 # Fungsi untuk memfilter data berdasarkan segmen
 def filter_segment(dataframe, segment_option):
-    if segment_option == "Entry-level":
+    if segment_option == "Entry Level":
         return dataframe[(dataframe["Price"] >= 8000) & (dataframe["Price"] <= 12000)]
-    elif segment_option == "Mid-range":
+    elif segment_option == "Mid range":
         return dataframe[(dataframe["Price"] >= 16000) & (dataframe["Price"] <= 19000)]
-    elif segment_option == "High-end":
+    elif segment_option == "High End":
         return dataframe[(dataframe["Price"] >= 30000) & (dataframe["Price"] <= 34000)]
     else:
         return dataframe # Semua data
 
 # Judul Dashboard
-st.title("💻 Laptop Price Prediction App")
+st.title("💻 Laptop Price Prediction")
 
-with st.form("Laptop Specs"):
-    st.header("🛠️ Input Laptop Specifications")
+# Form Input Spesifikasi Laptop
+st.header("🛠️ Input Laptop Specifications")
+processor_speed = st.slider("Processor Speed (GHz)", 1.0, 4.0, 2.5)
+ram_size = st.slider("RAM Size (GB)", 4, 64, 8)
+storage_capacity = st.slider("Storage Capacity (GB)", 128, 2000, 512)
+screen_size = st.slider("Screen Size (inch)", 10.0, 17.0, 15.6)
+weight = st.slider("Weight (kg)", 1.0, 5.0, 2.3)
+brand = st.selectbox("Brand", df["Brand"].unique())
 
-    processor_speed = st.slider("Processor Speed (GHz)", 1.0, 4.0, 2.5)
-    ram_size = st.slider("RAM Size (GB)", 4, 64, 8)
-    storage_capacity = st.slider("Storage Capacity (GB)", 128, 2000, 512)
-    screen_size = st.slider("Screen Size (inch)", 10.0, 17.0, 15.6)
-    weight = st.slider("Weight (kg)", 1.0, 5.0, 2.3)
-    brand = st.selectbox("Brand", df["Brand"].unique())
+# Prediksi Harga
+input_data = pd.DataFrame([{
+    "Brand": brand,
+    "Processor_Speed": processor_speed,
+    "RAM_Size": ram_size,
+    "Storage_Capacity": storage_capacity,
+    "Screen_Size": screen_size,
+    "Weight": weight
+}])
 
-    submitted = st.form_submit_button("Predict")
-
-    if submitted:
-        input_data = pd.DataFrame([{
-            "Brand": brand,
-            "Processor_Speed": processor_speed,
-            "RAM_Size": ram_size,
-            "Storage_Capacity": storage_capacity,
-            "Screen_Size": screen_size,
-            "Weight": weight
-        }])
-
-        predicted_price = model.predict(input_data)[0]
-        st.success(f"Predicted Price: Rp {predicted_price:,.0f}")
-        st.caption("Model : Lasso Regression")
+# Prediksi Harga
+predicted_price = model.predict(input_data)[0]
+st.success(f"Predicted Price : Rp {predicted_price:,.0f}")
+st.caption("Model : Lasso Regression")
 
 # Segmentasi Berdasarkan Prediksi Harga
 if 8000 <= predicted_price <= 12000:
-    segment = "Entry-level"
+    segment = "Entry Level"
 elif 16000 <= predicted_price <= 19000:
-    segment = "Mid-range"
+    segment = "Mid range"
 elif 30000 <= predicted_price <= 34000:
-    segment = "High-end"
+    segment = "High End"
 else:
     segment = "Out of defined range"
 
 # Eksplorasi Segmen Pasar (Menggunakan fungsi filter_segment)
 st.header("🔍 Explore Market Segments")
-segment_option_data = st.selectbox("Pilih Segmen Harga untuk Dataframe", ["Entry-level", "Mid-range", "High-end"])
+segment_option_data = st.selectbox("Segment", ["Entry Level", "Mid range", "High End"])
 filtered_df = filter_segment(df, segment_option_data)
 
 # Tampilkan Data Segmen
-st.subheader(f"📊 Segment: {segment_option_data}")
+st.subheader(f"📊 Segment : {segment_option_data}")
 st.dataframe(filtered_df)
 
 st.header("📈 Price Distribution by Segment")
 
 # Pilihan Segmen untuk Histogram (Menggunakan fungsi filter_segment)
-segment_option_plot = st.selectbox("Pilih Segmen Harga untuk Visualisasi", ["All", "Entry-level", "Mid-range", "High-end"])
+segment_option_plot = st.selectbox("Segment", ["All", "Entry Level", "Mid range", "High End"])
 plot_df = filter_segment(df, segment_option_plot)
 
 # Plot Histogram
